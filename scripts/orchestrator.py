2fef5c19eb54911b8ac821034e2a35d6d243050f
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-01-10 09:03:43 +0530

    Update scripts to change data storage schema

--- scripts/orchestrator.py
+++ scripts/orchestrator.py
@@ -22,9 +22,9 @@ def connect_to_db(user, password, dbname, host, port=8086):
         sys.exit(1)
 
 
-def write_dataframe(client, dataframe, mid, tags, collection):
-    tags.update({ 'mid': mid })
-    df = dataframe.set_index(pd.DatetimeIndex([datetime.now()]))
+def write_df(client, df, machine_df, tags, collection):
+    df = pd.concat([df, machine_df], axis=1)
+    df = df.set_index(pd.DatetimeIndex([datetime.now()]))
     saved = client.write_points(df, collection, tags, protocol='line')
     if not saved:
         print("Failed to save data to DB")
@@ -64,22 +64,11 @@ def read_disks():
     if not data:
         return list()
     return data.split("\n")[:-1]
-    
-def generate_mid():
+
+
+def fetch_machine_info():
     facter_output = subprocess.check_output(["facter", "--json"]).decode("utf-8")
-    machine_info = json.loads(facter_output)
-    useless_metadata = [
-        'facterversion', 'identity', 'system_uptime', 'virtual', 
-        'is_virtual', 'timezone', 'path', 'uptime', 'uniqueid', 'ps',
-        'rubyplatform', 'uptime_hours', 'gid', 'rubysitedir', 'id', 'uptime_seconds',
-        'uptime_days', 'rubyversion', 'hostname', 'fqdn', 'memoryfree_mb',
-        'memoryfree', 'swapfree_mb', 'swapfree'
-    ]
-    for key in useless_metadata:
-        machine_info.pop(key, None)
-    machine_info_json = json.dumps(machine_info, sort_keys = True).encode("utf-8")
-    mid = hashlib.md5(machine_info_json).hexdigest()
-    return mid, machine_info
+    return json.loads(facter_output)
 
 
 if __name__ == "__main__":
@@ -91,11 +80,9 @@ if __name__ == "__main__":
     host = os.environ['BLACKSWAN_HOST']
     client = connect_to_db(user, passwd, database, host)
 
-    # Generate a mid (machine id) and save the machine information
-    mid, machine_info = generate_mid()
-    if not client.query("select * from machine_information where \"mid\" = \'{}\'".format(mid)):
-        df = pd.DataFrame([machine_info])
-        write_dataframe(client, df, mid, dict(), 'machine_information')
+    # Generate a machine df from the fetched the machine information
+    machine_info = fetch_machine_info()
+    machine_df = pd.DataFrame([machine_info])
 
     # Gather and Save NAS CPU ST benchmarks
     npb_cpu_st_results = find_npb_cpu_st_tests()
@@ -103,9 +90,9 @@ if __name__ == "__main__":
         socketid = "socket{}".format(sno)
         for res in npb_cpu_st_results:
             if socketid in res:
-                dataframe = pd.read_csv(res)
-                dataframe['size'] = str(dataframe['size'])
-                write_dataframe(client, dataframe, mid, { 'socket': str(sno) }, 'npb_cpu_st')
+                df = pd.read_csv(res)
+                df['size'] = str(df['size'])
+                write_df(client, df, machine_df, { 'socket': str(sno) }, 'npb_cpu_st')
 
     # Gather and Save NAS CPU MT benchmarks
     npb_cpu_mt_results = find_npb_cpu_mt_tests()
@@ -113,21 +100,21 @@ if __name__ == "__main__":
         socketid = "socket{}".format(sno)
         for res in npb_cpu_mt_results:
             if socketid in res:
-                dataframe = pd.read_csv(res)
-                dataframe['size'] = str(dataframe['size'])
-                write_dataframe(client, dataframe, mid, { 'socket': str(sno) }, 'npb_cpu_mt')
+                df = pd.read_csv(res)
+                df['size'] = str(df['size'])
+                write_df(client, df, machine_df, { 'socket': str(sno) }, 'npb_cpu_mt')
 
     # Gather and Save membench benchmarks
     for sno in range(0, nsockets):
         filename = "membench_out_socket{}_dvfs.csv".format(sno)
-        dataframe = pd.read_csv(os.path.join(BASE_DIR, filename))
-        write_dataframe(client, dataframe, mid, { 'socket': str(sno) }, 'membench')
+        df = pd.read_csv(os.path.join(BASE_DIR, filename))
+        write_df(client, df, machine_df, { 'socket': str(sno) }, 'membench')
     
     # Gather and Save stream benchmarks
     for sno in range(0, nsockets):
         filename = "stream_out_socket{}_dvfs.csv".format(sno)
-        dataframe = pd.read_csv(os.path.join(BASE_DIR, filename))
-        write_dataframe(client, dataframe, mid, { 'socket': str(sno) }, 'stream')
+        df = pd.read_csv(os.path.join(BASE_DIR, filename))
+        write_df(client, df, machine_df, { 'socket': str(sno) }, 'stream')
 
     # Gather and Save fio benchmarks
     for device in read_disks():
@@ -135,44 +122,44 @@ if __name__ == "__main__":
         iodepth_4096_read_seq_benchmark = find_fio_benchmark_result_file(4096, 'read_seq', device)
 
         if iodepth_1_read_seq_benchmark:
-            dataframe = pd.read_csv(iodepth_1_read_seq_benchmark)
-            write_dataframe(client, dataframe, mid, { 'type': 'read_seq', 'io_depth': 1, 'device': device }, 'fio')
+            df = pd.read_csv(iodepth_1_read_seq_benchmark)
+            write_df(client, df, machine_df, { 'type': 'read_seq', 'io_depth': 1, 'device': device }, 'fio')
 
         if iodepth_4096_read_seq_benchmark:
-            dataframe = pd.read_csv(iodepth_4096_read_seq_benchmark)
-            write_dataframe(client, dataframe, mid, { 'type': 'read_seq', 'io_depth': 4096, 'device': device }, 'fio')
+            df = pd.read_csv(iodepth_4096_read_seq_benchmark)
+            write_df(client, df, machine_df, { 'type': 'read_seq', 'io_depth': 4096, 'device': device }, 'fio')
 
         iodepth_1_read_rand_benchmark = find_fio_benchmark_result_file(1, 'read_rand', device)
         iodepth_4096_read_rand_benchmark = find_fio_benchmark_result_file(4096, 'read_rand', device)
 
         if iodepth_1_read_rand_benchmark:
-            dataframe = pd.read_csv(iodepth_1_read_rand_benchmark)
-            write_dataframe(client, dataframe, mid, { 'type': 'read_rand', 'io_depth': 1, 'device': device }, 'fio')
+            df = pd.read_csv(iodepth_1_read_rand_benchmark)
+            write_df(client, df, machine_df, { 'type': 'read_rand', 'io_depth': 1, 'device': device }, 'fio')
 
         if iodepth_4096_read_rand_benchmark:
-            dataframe = pd.read_csv(iodepth_4096_read_rand_benchmark)
-            write_dataframe(client, dataframe, mid, { 'type': 'read_rand', 'io_depth': 4096, 'device': device }, 'fio')
+            df = pd.read_csv(iodepth_4096_read_rand_benchmark)
+            write_df(client, df, machine_df, { 'type': 'read_rand', 'io_depth': 4096, 'device': device }, 'fio')
 
         iodepth_1_write_seq_benchmark = find_fio_benchmark_result_file(1, 'write_seq', device)
         iodepth_4096_write_seq_benchmark = find_fio_benchmark_result_file(4096, 'write_seq', device)
 
         if iodepth_1_write_seq_benchmark:
-            dataframe = pd.read_csv(iodepth_1_write_seq_benchmark)
-            write_dataframe(client, dataframe, mid, { 'type': 'write_seq', 'io_depth': 1, 'device': device }, 'fio')
+            df = pd.read_csv(iodepth_1_write_seq_benchmark)
+            write_df(client, df, machine_df, { 'type': 'write_seq', 'io_depth': 1, 'device': device }, 'fio')
 
         if iodepth_4096_write_seq_benchmark:
-            dataframe = pd.read_csv(iodepth_4096_write_seq_benchmark)
-            write_dataframe(client, dataframe, mid, { 'type': 'write_seq', 'io_depth': 4096, 'device': device }, 'fio')
+            df = pd.read_csv(iodepth_4096_write_seq_benchmark)
+            write_df(client, df, machine_df, { 'type': 'write_seq', 'io_depth': 4096, 'device': device }, 'fio')
 
         iodepth_1_write_rand_benchmark = find_fio_benchmark_result_file(1, 'write_rand', device)
         iodepth_4096_write_rand_benchmark = find_fio_benchmark_result_file(4096, 'write_rand', device)
 
         if iodepth_1_write_rand_benchmark:
-            dataframe = pd.read_csv(iodepth_1_write_rand_benchmark)
-            write_dataframe(client, dataframe, mid, { 'type': 'write_rand', 'io_depth': 1, 'device': device }, 'fio')
+            df = pd.read_csv(iodepth_1_write_rand_benchmark)
+            write_df(client, df, machine_df, { 'type': 'write_rand', 'io_depth': 1, 'device': device }, 'fio')
 
         if iodepth_4096_write_rand_benchmark:
-            dataframe = pd.read_csv(iodepth_4096_write_rand_benchmark)
-            write_dataframe(client, dataframe, mid, { 'type': 'write_rand', 'io_depth': 4096, 'device': device }, 'fio')
+            df = pd.read_csv(iodepth_4096_write_rand_benchmark)
+            write_df(client, df, machine_df, { 'type': 'write_rand', 'io_depth': 4096, 'device': device }, 'fio')
 
     print('COMPLETED SUCCESSFULLY !')
